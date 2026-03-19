d2e34649e09166fcb52cfc059612e52a613d01cc
RazvanAndrei200526 <doruippu@gmail.com>
2026-03-19 12:13:06 +0200

    initial commit with boilerplate code i'll add a readme later with all the docs you guys need to read and to explain how everything works

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import app from "./app";
+
+const server = Bun.serve({
+	port: Bun.env.PORT || 3000,
+	hostname: "0.0.0.0",
+	fetch: app.fetch,
+});
+
+console.log(`Server running on http://localhost:${server.port}`);
