31091f2c66a17014728d6f835263736cba702c0d
CslnDeezY <dimacosulean11@gmail.com>
2026-03-26 18:59:50 +0200

    Created first variant of task model

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,26 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-export const usersTable = pgTable("users", {
-	id: integer().primaryKey().generatedAlwaysAsIdentity(),
-	name: varchar({ length: 255 }).notNull(),
-	age: integer().notNull(),
-	email: varchar({ length: 255 }).notNull().unique(),
-});
+import { pgTable, uuid, text, timestamp, boolean, pgEnum }  from "drizzle-orm/pg-core";
+import {InferSelectModel} from "drizzle-orm";
 
 //asta o sa fie sters e doar de test
+
+export const TaskStatus = pgEnum ("TaskStatus",
+	["OPEN", "CLAIMED", "DONE"]);
+
+//define the schema for the "task" table (#TD: poate fi imbunatatit \_(*_*)_/)
+export const task = pgTable("task", {
+	//auto-generated unique ID
+	id: uuid("id").defaultRandom().primaryKey(),
+	//title of the task, not null
+	title: text("title").notNull(),
+
+	//the status of the task, restricted to specific enum values ("open", "claimed", "done") (#TD: posibil)
+	status: TaskStatus("status").notNull().default("OPEN"),
+
+	//the date and time the task was created, not null, default to the current timestamp
+	createdAt: timestamp("createdAt").notNull().defaultNow(),
+	//the date and time of the last update. should be updated manually or via a database trigger whenever the row is modified
+	updatedAt: timestamp("updatedAt").notNull().defaultNow(),
+});
+
+export type TaskType = InferSelectModel<typeof task>;
+export type TaskStatus = "open" | "claimed" | "done";
