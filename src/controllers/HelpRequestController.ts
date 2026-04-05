b93adb31e7700b9247990116c3a6eda9ff3bbd1c
tasia786 <anastasiatofan786@gmail.com>
2026-04-05 19:07:12 +0300

    BE1-02 update2

--- src/controllers/HelpRequestController.ts
+++ src/controllers/HelpRequestController.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { Controller } from "../utils/Controller";
+import { Controller } from "../utils/controller";
 import { helpRequestService } from "../services/HelpRequestService";
 
 @Controller("/tasks")
--- src/services/HelpRequestService.ts
+++ src/services/HelpRequestService.ts
@@ -1,8 +1,7 @@
-import { helpRequestRepository } from "../repositories/HelpRequestRepository";
-import { RequestStatus, type HelpRequestType } from "../db/schema";
+import { helpRequestRepository, type CreateHelpRequestDTO } from "../db/repositories/helpRequests.repository";
 
 export class HelpRequestService {
-  async createHelpRequest(data: Partial<HelpRequestType>) {
+  async createHelpRequest(data: CreateHelpRequestDTO) {
     return await helpRequestRepository.create({
       ...data,
       status: "OPEN",
